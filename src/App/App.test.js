c134ceb52ee4a632c832c92a70e41234767dd7a8
Leon Marbukh <leonmarbukh@gmail.com>
2020-04-26 15:31:34 -0700

    removed favicons in test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,15 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faPlus,
-  faChevronLeft,
-  faTrashAlt,
-  faCheckDouble,
-} from "@fortawesome/free-solid-svg-icons";
 import App from "./App";
 
-library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble);
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
